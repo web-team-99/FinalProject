cefb05ecb572a34b8cda3695b74b521075080876
mohsenksr <mohsen.kassiri@live.com>
2021-02-18 07:01:25 +0330

    bug fixed

--- front/lib/providers/AuthenticationAPI.dart
+++ front/lib/providers/AuthenticationAPI.dart
@@ -8,7 +8,7 @@ import 'package:test_url/models/user.dart';
 class AuthenticationAPI {
   Future<User> sendSignUpRequest(User user) async {
     var response;
-    Map<String, String> headers = {"Content-type": "multipart/form-data;"};
+    Map<String, String> headers = {"Content-type": "multipart/form-data"};
     String body =
         "name=${user.name}&lname=${user.lastName}&password=${user.password}&email=${user.email}&phone=${user.phone}";
     try {
@@ -39,7 +39,7 @@ class AuthenticationAPI {
 
   Future<User> sendLoginRequest(String email, String password) async {
     var response;
-    Map<String, String> headers = {"Content-type": "multipart/form-data;"};
+    Map<String, String> headers = {"Content-type": "multipart/form-data"};
     String body = "password=$password&email=$email";
     try {
       response = http.post(loginUrl, headers: headers, body: body);
